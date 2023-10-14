20a5a454140a22b2f351d8d538d16c615a83f292
Fernando Zhapa <ferzcam@gmail.com>
2023-10-15 01:02:04 +0300

    semantic files

--- evaluate_sem.py
+++ evaluate_sem.py
@@ -15,53 +15,38 @@ from scipy import sparse
 import math
 from utils import FUNC_DICT, Ontology, NAMESPACES, EXP_CODES
 from matplotlib import pyplot as plt
+import os
+import pickle as pkl
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
-@ck.command()
-@ck.option(
-    '--data-root', '-dr', default='data',
-    help='Prediction model')
-@ck.option(
-    '--ont', '-ont', default='mf',
-    help='Prediction model')
-@ck.option(
-    '--model', '-m', default='deepgo2',
+def test(data_root, ont, model, combine, alpha, tex_output, aggregator, cml_logger):
 
-    help='Prediction model')
-@ck.option(
-    '--combine', '-c', is_flag=True,
-    help='Prediction model')
-@ck.option(
-    '--alpha', '-a', default=0.50,
-    help='Combining weight')
-@ck.option(
-    "--aggregator", "-agg", default="mean", type=ck.Choice(["mean", "max", "min", "median"]),
-    help="Aggregation function for combining scores")
-@ck.option(
-    '--num-preds', '-np', default=50,
-    help='Combining weight')
-def main(data_root, ont, model, combine, alpha, aggregator, num_preds):
-    train_data_file = f'{data_root}/{ont}/train_data.pkl'
-    valid_data_file = f'{data_root}/{ont}/valid_data.pkl'
+    prop_annots_file = f"{data_root}/{ont}/propagated_annotations.pkl"
     test_data_file = f'{data_root}/{ont}/predictions_{model}.pkl'
-    diam_data_file = f'{data_root}/{ont}/test_data_diam.pkl'
     terms_file = f'{data_root}/{ont}/terms.pkl'
-    go_rels = Ontology(f'data/go-basic.obo', with_rels=True)
+    go_rels = Ontology(f'{data_root}/go-basic.obo', with_rels=True)
     terms_df = pd.read_pickle(terms_file)
     terms = terms_df['gos'].values.flatten()
     terms_dict = {v: i for i, v in enumerate(terms)}
 
-    train_df = pd.read_pickle(train_data_file)
-    valid_df = pd.read_pickle(valid_data_file)
-    train_df = pd.concat([train_df, valid_df])
-    test_df = pd.read_pickle(test_data_file)
-    diam_df = pd.read_pickle(diam_data_file)
+    if not os.path.exists(prop_annots_file):
+        train_data_file = f'{data_root}/{ont}/train_data.pkl'
+        valid_data_file = f'{data_root}/{ont}/valid_data.pkl'
 
+        train_df = pd.read_pickle(train_data_file)
+        valid_df = pd.read_pickle(valid_data_file)
+        train_df = pd.concat([train_df, valid_df])
+        annotations = train_df['prop_annotations'].values
+        annotations = list(map(lambda x: set(x), annotations))
+        pkl.dump(annotations, open(prop_annots_file, 'wb'))
+    else:
+        annotations = pkl.load(open(prop_annots_file, 'rb'))
+        
+    test_df = pd.read_pickle(test_data_file)
+    # diam_df = pd.read_pickle(diam_data_file)
     
-    annotations = train_df['prop_annotations'].values
-    annotations = list(map(lambda x: set(x), annotations))
     test_annotations = test_df['prop_annotations'].values
     test_annotations = list(map(lambda x: set(x), test_annotations))
     go_rels.calculate_ic(annotations + test_annotations)
@@ -73,16 +58,16 @@ def main(data_root, ont, model, combine, alpha, aggregator, num_preds):
     
     # Combine scores for diamond and deepgo
     eval_preds = []
-    
+ 
     for i, row in enumerate(test_df.itertuples()):
-        diam_preds = np.zeros((len(terms),), dtype=np.float32)
-        for go_id, score in diam_df.iloc[i]['diam_preds'].items():
-            if go_id in terms_dict:
-                diam_preds[terms_dict[go_id]] = score
-
-        row_preds = getattr(row, f"preds_{aggregator}")
+        row_preds = getattr(row, f"preds_{aggregator}")  #row[f"preds_{aggregator}"]
         if combine:
-            preds = diam_preds * alpha + row_preds * (1 - alpha)
+            diam_preds = np.zeros((len(terms),), dtype=np.float32)
+            for go_id, score in diam_df.iloc[i]['diam_preds'].items():
+                if go_id in terms_dict:
+                    diam_preds[terms_dict[go_id]] = score
+        
+                preds = diam_preds * alpha + row_preds * (1 - alpha)
         else:
             preds = row_preds
         eval_preds.append(preds)
@@ -176,6 +161,45 @@ def main(data_root, ont, model, combine, alpha, aggregator, num_preds):
     print(f'AUPR: {aupr:0.3f}')
     print(f'AVGIC: {avgic:0.3f}')
 
+    cml_logger.log({
+        f"fmax_{aggregator}": fmax,
+        f"smin_{aggregator}": smin,
+        f"aupr_{aggregator}": aupr,
+        f"avg_auc_{aggregator}": avg_auc,
+        f"wfmax_{aggregator}": wfmax,
+        f"avgic_{aggregator}": avgic,
+        f"threshold_{aggregator}": tmax,
+        f"w_threshold_{aggregator}": wtmax,
+        f"spec_{aggregator}": fmax_spec_match,
+        f"combine": combine      
+    })
+
+
+
+    
+    # wandb.finish()
+
+    
+    # cml_logger.report_single_value("fmax", fmax)
+    # cml_logger.report_single_value("smin", smin)
+    # cml_logger.report_single_value("aupr", aupr)
+    # cml_logger.report_single_value("avg_auc", avg_auc)
+    # cml_logger.report_single_value("wfmax", wfmax)
+    # cml_logger.report_single_value("avgic", avgic)
+    # cml_logger.report_single_value("threshold", tmax)
+    # cml_logger.report_single_value("w_threshold", wtmax)
+    # cml_logger.report_single_value("spec", fmax_spec_match)
+    # cml_logger.report_single_value("combine", combine)
+
+    # cml_logger.flush()
+    
+
+    
+    if tex_output:
+        tex = "& "
+        tex += f"{fmax:0.3f} & {smin:0.3f} & {aupr:0.3f} & {avg_auc:0.3f} \\\\"
+        print(tex)
+        
     plt.figure()
     lw = 2
     plt.plot(recalls, precisions, color='darkorange',
@@ -190,6 +214,9 @@ def main(data_root, ont, model, combine, alpha, aggregator, num_preds):
     df = pd.DataFrame({'precisions': precisions, 'recalls': recalls})
     df.to_pickle(f'{data_root}/{ont}/pr_{model}.pkl')
 
+    
+
+    
 def compute_roc(labels, preds):
     # Compute ROC curve and ROC area for each class
     fpr, tpr, _ = roc_curve(labels.flatten(), preds.flatten())
